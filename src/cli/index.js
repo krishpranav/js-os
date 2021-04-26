ebb005a7bf9ff729879e715d782423e1a07694c8
krishpranav <krisna.pranav@gmail.com>
2021-04-26 17:51:26 +0530

    updated index.js

---
+++ src/cli/index.js
@@ -0,0 +1,13 @@
+/*
+* @author krishpranav
+*/
+
+// path
+const path = require('path');
+
+module.exports = {
+  discover: [
+    path.resolve(__dirname, '../packages') // OS.js/src/packages
+  ],
+  tasks: []
+};
